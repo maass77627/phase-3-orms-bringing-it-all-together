74550e94eb66d727b8979ad25fa6642d9e33cc31
kelly_maass <kmaass77627@gmail.com>
2025-03-25 20:25:20 -0500

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,4 +35,52 @@ class Dog
         self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
      end
 
+
+     def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+        dog
+      end
+
+      def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+      end
+
+      def self.all
+        sql = <<-SQL
+          SELECT *
+          FROM dogs;
+        SQL
+    
+        DB[:conn].execute(sql).map do |row|
+          self.new_from_db(row)
+        end
+      end
+
+      
+      def self.find_by_name(name)
+        sql = <<-SQL
+          SELECT *
+          FROM dogs
+          WHERE dogs.name = ?
+          LIMIT 1;
+        SQL
+    
+        DB[:conn].execute(sql, name).map do |row|
+          self.new_from_db(row)
+        end.first
+      end
+    
+      def self.find(id)
+        sql = <<-SQL
+          SELECT *
+          FROM dogs
+          WHERE dogs.id = ?
+          LIMIT 1;
+        SQL
+    
+        DB[:conn].execute(sql, id).map do |row|
+          self.new_from_db(row)
+        end.first
+      end
 end
